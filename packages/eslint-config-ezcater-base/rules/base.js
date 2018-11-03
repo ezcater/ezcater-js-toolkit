2ff87d6f1a2fa71b87c98ff6862e81dade401d3a
Craig Cavalier <craigcav@gmail.com>
2018-11-02 23:44:43 -0400

    update rules to more closely align with current conventions

--- packages/eslint-config-ezcater-base/rules/base.js
+++ packages/eslint-config-ezcater-base/rules/base.js
@@ -6,5 +6,39 @@ module.exports = {
     'no-extra-bind': 'error',
     'no-implicit-globals': 'error',
     'prefer-promise-reject-errors': 'error',
+    curly: ['error', 'multi-or-nest'],
+    'object-curly-spacing': ['error', 'never'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'object-curly-newline': ['error', {consistent: true}],
+    'operator-linebreak': ['error', 'after'],
+    'quote-props': ['error', 'as-needed'],
+    'nonblock-statement-body-position': 'off',
+    'max-len': [
+      'error',
+      100,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'implicit-arrow-linebreak': 'off',
+    // causes "Cannot read property 'range' of null". See: https://github.com/babel/babel-eslint/issues/530
+    indent: 'off',
+    // causes "Cannot read property 'range' of null". See: https://github.com/babel/babel-eslint/issues/530
+    'template-curly-spacing': 'off',
+    'arrow-parens': ['error', 'as-needed'],
   },
 };
