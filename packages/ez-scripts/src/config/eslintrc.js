a3bcea5c056d6eda84d91e8f324aa12920522df0
Craig Cavalier <craigcav@gmail.com>
2019-04-02 10:32:02 -0400

    update to use shared typescript rules

--- packages/ez-scripts/src/config/eslintrc.js
+++ packages/ez-scripts/src/config/eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    require.resolve('eslint-config-ezcater-react'),
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/indent': 'off',
-  },
-  settings: {
-    parser: 'typescript-eslint-parser',
-    plugins: ['import'],
-    'import/resolver': {
-      typescript: {},
-    },
-  },
+  extends: [require.resolve('eslint-config-ezcater-typescript')],
 };
