d6213b2c91c1264589822b41a140db1c6a8ba8f8
Craig Cavalier <craigcav@gmail.com>
2019-10-03 13:59:50 -0400

    disabled no-inferrable-types

--- packages/eslint-config-ezcater-typescript/rules/typescript.js
+++ packages/eslint-config-ezcater-typescript/rules/typescript.js
@@ -7,5 +7,6 @@ module.exports = {
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
   },
 };
