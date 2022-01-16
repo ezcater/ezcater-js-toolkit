3d8108db71908d9f20d022805eec28bf44274117
Julian Flynn <julian.flynn@ezcater.com>
2022-01-16 00:54:46 -0500

    fix: eslintConfig and eslintIgnore being skipped
    What did we change?
    
    The package.json property `eslintConfig` and `eslintIgnore` are not read
    by the `ez-scripts lint` command.
    
    Why are we doing this?
    
    Bugfix

---
+++ packages/ez-scripts/jest.config.js
@@ -0,0 +1,8 @@
+// @ts-check
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  name: 'ez-scripts',
+  displayName: 'ez-scripts',
+  roots: ['<rootDir>/src'],
+  resetMocks: true,
+};
